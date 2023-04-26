6d0f29de419840b3ed521a7fd9d1ee3f4894df6f
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2023-04-26 12:11:57 -0300

    (feat): input validation

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,78 +1,128 @@
 <template>
     <div class="row justify-content-md-center form-container mt-3">
-        <div class="col col-lg-4">
-            teste
+
+        <h1 class="text-center bg-blue-accent-1 mb-5 p-1 rounded-1">Contate-nos</h1>
+
+        <div v-if="errorOnSubmit" class="alert alert-danger text-center" role="alert">
+            Preencha todos os campos de forma válida!
         </div>
 
-        <div class="col">
-            <div class="row g-3">
-                <div class="col-md-12">
-                    <label for="inputNome" class="form-label">Nome</label>
-                    <input type="text" class="form-control" id="inputNome"
-                           :class="{ 'is-valid': nomeValido, 'is-invalid': nomeInvalido && nomeTocado }" v-model="nome"
-                           @input="nomeTocado = true">
-                    <div class="invalid-feedback" v-if="nomeTocado && nomeInvalido">
-                        O nome deve ter pelo menos 2 caracteres e não deve conter números.
-                    </div>
-                </div>
+        <div v-if="errorOnSubmit === false" class="alert alert-success text-center" role="alert">
+            Obrigado, logo entraremos em contato!
+        </div>
 
-                <div class="col-md-12">
-                    <label for="inputEmail" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail"
-                           :class="{ 'is-valid': emailValido, 'is-invalid': emailInvalido && emailTocado }"
-                           v-model="email" @input="emailTocado = true">
-                    <div class="invalid-feedback" v-if="emailTocado && emailInvalido">
-                        Por favor, digite um endereço de email válido.
-                    </div>
-                </div>
+        <div class="d-flex" :class="{ 'flex-column': $vuetify.display.sm }">
 
-                <div class="col-12">
-                    <label for="inputTelefone" class="form-label">Telefone</label>
-                    <input type="text" class="form-control" id="inputTelefone" placeholder="Telefone"
-                           v-mask="'(##) #####-####'"
-                           :class="{ 'is-valid': telefoneValido, 'is-invalid': telefoneInvalido && telefoneTocado }"
-                           v-model="telefone" @input="(telefoneTocado = true)"
-                    >
-                    <div class="invalid-feedback" v-if="telefoneTocado && telefoneInvalido">
-                        Por favor, digite um número de telefone válido (formato: (xx) xxxxx-xxxx).
-                    </div>
-                </div>
 
-                <div class="col-12">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
-                    </div>
-                </div>
+            <div class="col col-lg-4">
 
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+            <!-- TODO - contacts -->
+
+                teste
+
+            </div>
+
+            <div class="col">
+
+                <div class="row g-3 d-flex">
+
+                    <div class="col-md-12 form-floating">
+
+
+                        <input
+                                type="text"
+                                class="form-control"
+                                id="inputNome"
+                                placeholder="nome"
+                                :class="{ 'is-valid': nomeValido, 'is-invalid': errorOnSubmit || nomeInvalido && nomeTocado }"
+                                v-model="nome"
+                                @input="nomeTocado = true"
+                        >
+                        <label for="inputNome">Nome</label>
+
+                        <div class="invalid-feedback" v-if="nomeTocado && nomeInvalido">
+                            O nome deve ter pelo menos 2 caracteres e não deve conter números.
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-12 form-floating">
+
+                        <input
+                                type="email"
+                                class="form-control"
+                                id="inputEmail"
+                                placeholder="name@example.com"
+                                :class="{ 'is-valid': emailValido, 'is-invalid': errorOnSubmit || emailInvalido && emailTocado }"
+                                v-model="email"
+                                @input="emailTocado = true"
+                        >
+                        <label for="inputNome">Email</label>
+
+                        <div class="invalid-feedback" v-if="emailTocado && emailInvalido">
+                            Por favor, digite um endereço de email válido.
+                        </div>
+                    </div>
+
+                    <div class="col-12  mb-3  form-floating">
+                        <input type="text" class="form-control" id="inputTelefone" placeholder="Telefone"
+                               v-mask="'(##) #####-####'"
+                               :class="{ 'is-valid': telefoneValido, 'is-invalid': errorOnSubmit || telefoneInvalido && telefoneTocado }"
+                               v-model="telefone" @input="(telefoneTocado = true)"
+                        >
+
+                        <label for="inputTelefone" class="form-label">Telefone</label>
+
+
+                        <div class="invalid-feedback" v-if="telefoneTocado && telefoneInvalido">
+                            Por favor, digite um número de telefone válido (formato: (xx) xxxxx-xxxx).
+                        </div>
+                    </div>
+
+                    <div class="col-12 d-flex justify-center align-center">
+                        <button type="submit" class="btn btn-primary" :class="{'btn-danger': errorOnSubmit, 'btn-success': errorOnSubmit === false}" @click="submit()">Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
 <script>
 
 
+import {vuetify} from "@/plugins";
+
 export default {
     name: 'FormComponent',
-    components: {
-    },
+    components: {},
     data() {
         return {
             nome: '',
             email: '',
             telefone: '',
+            errorOnSubmit: null,
             nomeTocado: false,
             emailTocado: false,
             telefoneTocado: false
         };
     },
+    methods: {
+        submit() {
+
+            this.errorOnSubmit = !this.nomeValido && !this.emailValido && !this.telefoneValido;
+
+            setTimeout(() => {
+                this.errorOnSubmit = '';
+            }, 2500);
+
+        }
+    },
     computed: {
+        vuetify() {
+            return vuetify
+        },
         nomeValido() {
             return !this.nomeInvalido && this.nome.trim().length > 0 && !this.nomeComNumeros;
         },
