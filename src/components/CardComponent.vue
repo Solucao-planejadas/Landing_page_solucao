a49fe2cd94fc614ea0cfde0879288961ab3bd50b
WilliamFraga21 <william.fraga@fatec.sp.gov.br>
2023-04-25 21:48:41 -0300

    (fix): Botão do Card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,62 +1,73 @@
 <template>
-    <div class="products gap-3">
+  <div class="products gap-3">
+    <div class="row">
+      <div
+        class="col-sm-4"
+        v-for="(card, index) in cards"
+        v-bind:key="card.title"
+      >
+        <div class="card">
+          <img :src="card.src" class="card-img-top" :alt="card.title" />
+          <div class="card-body">
 
-        <div class="row">
+            <h5 class="card-title">{{ card.title }}</h5>
+            <p class="d-none">{{ index }}</p>
 
-            <div class="col-sm-4" v-for="(card, index) in cards" v-bind:key="card.title">
+            <div class="d-flex justify-content-between align-center">
+              <button class="btn btn-sm">
+                Ver coleção
+              </button>
 
-                <div class="card">
-                    <img :src="card.src" class="card-img-top" :alt="card.title">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ card.title }}</h5>
-                        <p class="d-none">{{ index }}</p>
-
-                        <button class="btn btn-sm">Ver coleção</button>
-
-                        <div v-if="showCollapse">
-                            <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="collapse"
-                                    :data-bs-target="'#collapse' + index" aria-expanded="false"
-                                    :aria-controls="'#collapse' + index">
-                                Leia Mais
-                            </button>
-
-                            <div class="collapse" :id="'collapse' + index">
-                                <div class="">
-                                    {{ card.text }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div v-if="showCollapse">
+                <button
+                  class="btn btn-primary btn-sm"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  :data-bs-target="'#collapse' + index"
+                  aria-expanded="false"
+                  :aria-controls="'#collapse' + index"
+                >
+                  Leia Mais
+                </button>
+              </div>
             </div>
+
+            <div class="collapse p-3" :id="'collapse' + index">
+              <div class="">
+                {{ card.text }}
+              </div>
+            </div>
+
+
+          </div>
         </div>
-
-
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'CardComponent',
-    props: {
-        cards: {
-            required: true,
-            type: Array
-        },
-        showCollapse: {
-            required: false,
-            default: true,
-            type: Boolean
-        }
-    }
-}
+  name: "CardComponent",
+  props: {
+    cards: {
+      required: true,
+      type: Array,
+    },
+    showCollapse: {
+      required: false,
+      default: true,
+      type: Boolean,
+    },
+  },
+};
 </script>
 <style scoped>
 .products {
-    background: var(--tertiary-gray);
+  background: var(--tertiary-gray);
 }
 
 .btn {
-    background: var(--primary-color);
-    color: var(--white-color);
+  background: var(--primary-color);
+  color: var(--white-color);
 }
 </style>
