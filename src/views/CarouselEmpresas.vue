1de6db416be4f9923175cc779b941e00b5e7d2d9
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2023-04-26 22:38:28 -0300

    (fix): responsividade do slide

--- src/views/CarouselEmpresas.vue
+++ src/views/CarouselEmpresas.vue
@@ -1,18 +1,27 @@
 <template>
-    <CarouselCard ref="carouselCardRef" :interval="3000" :autoplay="true" height="260px" pause-on-hover="" type="card" arrow="hover">
-        <CarouselCardItem class="bg-transparent" v-for="i in 6" :key="i" :name="`cc_${i}`">
-            <img class="rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAK80nyUvD3O-2NYEbRYLi0imoXL7sK2lJxFil_lHp_gtKRNgGt_ebXqQOmyTehdBGZGE&usqp=CAU" alt="">
-        </CarouselCardItem>
-    </CarouselCard>
+    <div class="container">
+
+        <CarouselCard ref="carouselCardRef" :interval="3000" :autoplay="true" height="260px" pause-on-hover=""
+                      type="card" arrow="hover">
+            <CarouselCardItem class="bg-transparent" :class="{'d-flex justify-center align-center': !$vuetify.display.xs || !$vuetify.display.sm}" v-for="i in 6" :key="i" :name="`cc_${i}`">
+                <img class="rounded-circle"
+                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAK80nyUvD3O-2NYEbRYLi0imoXL7sK2lJxFil_lHp_gtKRNgGt_ebXqQOmyTehdBGZGE&usqp=CAU"
+                     alt="">
+            </CarouselCardItem>
+        </CarouselCard>
+
+    </div>
+
 
 </template>
 <script>
 
-import { CarouselCard, CarouselCardItem } from 'vue-carousel-card'
+import {CarouselCard, CarouselCardItem} from 'vue-carousel-card'
 import 'vue-carousel-card/styles/index.css'
+
 export default {
     name: 'CarouselEmpresas',
-    components: { CarouselCard, CarouselCardItem }
+    components: {CarouselCard, CarouselCardItem}
 }
 </script>
 
