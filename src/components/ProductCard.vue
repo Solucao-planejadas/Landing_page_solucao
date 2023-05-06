a9d3be6e519b5f24a20882408053bf033b93a66d
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2023-05-06 15:22:25 -0300

    (feat): add scroll no card overlay e o scroll tipo mobile quando tem muito texto

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,15 +6,11 @@
             <div class="card-body">
                 <p class="card-text">{{ item.title }}</p>
             </div>
-
             <div :title="item.description">
                 <div class="card-overlay" @mouseover="showTooltip = true" @mouseout="showTooltip = false">
                     <div class="card-overlay-text">
                         <p class="card-overlay-title">{{ item.title }}</p>
-                        <div class="card-overlay-info">
-                            <div class="card-overlay-description">{{ item.title }}</div>
-                            <div class="card-overlay-price">{{ item.title }}</div>
-                        </div>
+                        <div class="card-overlay-description">{{ item.description }}</div>
                     </div>
                 </div>
             </div>
@@ -48,7 +44,7 @@ img:hover {
     bottom: 0;
     left: 0;
     right: 0;
-    background-color: rgba(0, 0, 0, 0.6);
+    background-color: rgba(0, 0, 0, 0.8);
     opacity: 0;
     transition: opacity 0.2s ease-in-out;
 }
@@ -58,34 +54,48 @@ img:hover {
 }
 
 .card-overlay-text {
+    overflow-y: auto;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     text-align: center;
-    color: #fff;
+    color: var(--white-color);
+    width: 100%;
+    height: calc(100% - 3rem);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: .9rem;
 }
 
 .card-overlay-title {
-    font-size: 1.2rem;
+    font-size: 1rem;
     font-weight: bold;
     text-transform: uppercase;
     letter-spacing: 0.1rem;
-}
-
-.card-overlay-info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    margin-bottom: 1rem;
 }
 
 .card-overlay-description {
-    font-size: 1rem;
-    margin-bottom: 5px;
+    margin: 0;
+    max-height: 150px; /* ou qualquer valor que você achar adequado */
+    overflow-y: auto;
+
 }
 
-.card-overlay-price {
-    font-size: 1.2rem;
-    font-weight: bold;
+::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: #c4c4c4;
+    border-radius: 3px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background-color: #a0a0a0;
 }
 </style>
