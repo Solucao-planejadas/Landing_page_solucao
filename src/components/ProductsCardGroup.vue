1ef826b8c3732a51879322e95b3eec131e4c1bda
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2023-05-05 22:10:12 -0300

    (fix): fix erro da column quando é uma tela de celular com menos de 768

--- src/components/ProductsCardGroup.vue
+++ src/components/ProductsCardGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="tab-pane fade" :class="{'show active': isButtonActive}" :id="nav" role="tabpanel" :aria-labelledby="ariaLabelledby">
-        <div class="card-group d-flex justify-center align-center mt-3 gap-4" :class="{'flex-column': $vuetify.display.xs}">
+        <div class="card-group d-flex justify-center align-center mt-3 gap-4" :class="{'flex-column': $vuetify.display.xs || windowWidth < 768 }">
             <ProductCard :contentData="contentDataProntaEntrega"/>
         </div>
     </div>
@@ -11,6 +11,11 @@ import ProductCard from "@/components/ProductCard.vue"
 export default {
     name: 'ProductsCardGroup',
     components: {ProductCard},
+    data() {
+        return {
+            windowWidth: window.innerWidth
+        }
+    },
     props: {
         contentDataProntaEntrega: {},
         nav: {},
