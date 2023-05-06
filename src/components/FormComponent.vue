04855dc5d5646ad05ae50a03731137f2fca5fde1
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2023-05-05 22:32:21 -0300

    separar o p1 do rounded

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row justify-content-md-center form-container mt-3 pt-10 pb-10" id="orcamento">
         <div class=" d-flex justify-center align-center">
-            <h1 class="text-center bg-blue-accent-1 w-50 mb-5 p-1rounded-1 text-white">Contate-nos</h1>
+            <h1 class="text-center bg-blue-accent-1 w-50 mb-5 p-1 rounded-1 text-white">Contate-nos</h1>
         </div>
 
         <div v-if="errorOnSubmit" class="alert alert-danger text-center" role="alert">
