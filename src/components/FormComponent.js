1f186e8e6c5c7f4f8d1dbb12f0ff3026f0855381
Marcone-Santos1 <marconemagalhes203@gmail.com>
2023-09-29 19:49:56 -0300

    teste cors

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -50,7 +50,6 @@ export default {
                     this.user, {
                         headers: {
                             'Content-Type': 'application/json;charset=UTF-8',
-                            "Access-Control-Allow-Origin": "*",
                         }
                     })
                     .then((response) => {
